5c6c9674116e6d191f87f2699595faf6ff7d1f1c
Rupa Sree <rupasree045@gmail.com>
2019-07-19 10:30:34 +0530

    Update Actor.java
    comments added

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,17 +1,17 @@
 package com.stackroute.domain;
-
+ //Actor class
 public class Actor {
-
+//Member variables of Actor class
     private String name;
     private String gender;
     private int age;
-
+    //Actor Constructor 
     public Actor(String name, String gender, int age) {
         this.name = name;
         this.gender = gender;
         this.age = age;
     }
-
+     // getters and setters of actor class
     public String getName() {
         return name;
     }
