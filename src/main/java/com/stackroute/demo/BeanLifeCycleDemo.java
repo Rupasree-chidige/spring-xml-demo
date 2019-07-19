252a36972942bffa573059bebae079c89584edd0
Rupa Sree <rupasree045@gmail.com>
2019-07-19 10:44:01 +0530

    Update BeanLifeCycleDemo.java
    Added Comments

--- src/main/java/com/stackroute/demo/BeanLifeCycleDemo.java
+++ src/main/java/com/stackroute/demo/BeanLifeCycleDemo.java
@@ -5,22 +5,17 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+//BeanLifeCycleDemo implements InitializingBean And DisposableBean
 public class BeanLifeCycleDemo implements InitializingBean, DisposableBean {
 
 
 
     public  static  void main(String args []) {
-
+        //Application context
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("BeanFile.xml");
-
-        ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
         BeanLifeCycleDemo beanLifeCycleDemo = (BeanLifeCycleDemo) applicationContext.getBean("lifeCycle");
-
-
-
-
-
-    }
+ }
+    
     public void coustomInit ()
     {
         System.out.println("Initialization");
