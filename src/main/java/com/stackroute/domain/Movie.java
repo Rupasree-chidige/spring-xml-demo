f1c945a9dbdbf6c08dd676e9d78cdfa2907b4e28
Rupa Sree <rupasree045@gmail.com>
2019-07-19 10:10:01 +0530

    Update Movie.java
    comments added

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,24 +1,24 @@
 package com.stackroute.domain;
-
+//Class Movie 
 public class Movie {
     Actor actor;
 
-
+//Constructor for Movie
     public Movie(Actor actor) {
         this.actor=actor;
     }
-
-
 
     @Override
     public String toString()
     {
         return actor.getName() +actor.getGender()+actor.getAge();
     }
+    
+    //setters and getters for Actor class
     public Actor getActor() {
         return actor;
     }
-
+  
     public void setActor(Actor actor) {
         this.actor = actor;
     }
