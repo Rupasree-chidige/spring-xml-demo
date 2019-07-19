87aabf97c720e3919c2a3c495521684516114167
Rupa Sree <rupasree045@gmail.com>
2019-07-19 10:28:34 +0530

    Update Main.java
    added comments

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -14,16 +14,17 @@ import org.springframework.core.io.ClassPathResource;
 public class Main {
  public  static  void  main(String[] args)
  {
+    //Object creation of Movie class
      Movie movie=new Movie();
-
+    //using Application Context 
      ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
      movie.setApplicationContext(applicationContext);
      System.out.println(movie.getApplicationContext().getBean("movie1",Movie.class).toString());
-
+    //Using Bean factory
      BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
      movie.setBeanFactory(beanFactory);
      System.out.println(movie.getBeanFactory().getBean("movie2",Movie.class).toString());
-
+     //using bean name
      movie.setBeanName("movie2");
      System.out.println(movie.getApplicationContext().getBean(movie.getBeanName(),Movie.class).toString());
 
